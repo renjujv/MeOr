9ace23bb1af3f743b3bb6086d2f26cb8c26d61b6
renjujv <renjujv@gmail.com>
2015-07-16 18:02:44 +0530

    modified about page

--- src/main/UI/About.java
+++ src/main/UI/About.java
@@ -10,6 +10,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import java.awt.Toolkit;
+import javax.swing.JScrollPane;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
 
 
 /**
@@ -19,65 +22,77 @@ import java.awt.Toolkit;
 @SuppressWarnings("serial")
 public class About extends JFrame
 {	
-	private JPanel aboutPage;
+	private double version=0.1;
+	private JPanel aboutpanel;
 	public static void main(String[] args)
 		{
 			EventQueue.invokeLater(new Runnable() {
 				public void run()
 					{
-							About frame = new About();
-							frame.setVisible(true);
+							About aboutframe = new About();
+							aboutframe.setVisible(true);
 					}
 			});
 		}
-	
+	//Constructor
 	public About() {
 		setType(Type.POPUP);
 		setTitle("About");
 		setResizable(false);
-		setOpacity(0.5f);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(About.class.getResource("/main/UI/Meor_icon.png")));
 		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		aboutPage = new JPanel();
-		aboutPage.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(aboutPage);
-		aboutPage.setLayout(null);
+		initUI();
 		setElements();
 		
 	}
+	//Initialize the panel
+	public void initUI(){
+		aboutpanel = new JPanel();
+		aboutpanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(aboutpanel);
+		aboutpanel.setLayout(null);
+	}
+	//Insert Elements
 	public void setElements(){
+		//Icon
 		JLabel meorIcon = new JLabel("MeOr");
 		meorIcon.setToolTipText("MeOr");
-		meorIcon.setLabelFor(aboutPage);
+		meorIcon.setLabelFor(aboutpanel);
 		meorIcon.setBounds(0, 0, 185, 160);
-		meorIcon.setIcon(new ImageIcon(About.class
-				.getResource("/main/UI/Meor_icon.png")));
+		meorIcon.setIcon(new ImageIcon(About.class.getResource("/main/UI/Meor_icon.png")));
 		getContentPane().add(meorIcon);
-
+		
+		//Text labels start
 		JLabel lblpage = new JLabel("MeOr - Media Organiser");
 		lblpage.setFont(new Font("Arial Black", Font.PLAIN, 18));
 		lblpage.setBounds(139, 8, 231, 25);
-		aboutPage.add(lblpage);
+		aboutpanel.add(lblpage);
 
-		JLabel lblversion = new JLabel("version 0.0.1");
+		JLabel lblversion = new JLabel("version "+version);
 		lblversion.setBounds(175, 44, 80, 14);
-		aboutPage.add(lblversion);
+		aboutpanel.add(lblversion);
 
-		JLabel lblcopyrightnote = new JLabel("Copyright @ 2013");
+		JLabel lblcopyrightnote = new JLabel("Copyright - RenJOuS©");
 		lblcopyrightnote.setBounds(175, 67, 139, 20);
-		aboutPage.add(lblcopyrightnote);
+		aboutpanel.add(lblcopyrightnote);
+		//Text labels end
 		
+		//Credits text
 		JTextArea txtMEOR = new JTextArea();
-		txtMEOR.setBounds(10, 150, 425, 110);
-		aboutPage.add(txtMEOR);
-		txtMEOR.setEditable(false); // text is not editable and it need not
-				// be,right??
-		txtMEOR.setText("Media Organizer is an Open-Source software\ndistribution free of charge"
-				+ "under the terms of GNU\nGeneral Public License.\r\nDeveloped by:\r\nRenju Jose,Rohan "
-				+ "Philip,Abijith N V,Muhammad Rafeek S R\n");
+		txtMEOR.setEditable(false);
+		txtMEOR.setText("Credits:\nMedia Organizer is an Open-Source software distribution free of charge"
+				+ "\nunder the terms of GNU General Public License.\r\nDeveloped by:\r\nRenju Jose,\nRohan "
+				+ "Philip,\nAbijith N V,\nMuhammad Rafeek S R");
 		txtMEOR.setLineWrap(true); // for setting text wrapping
 		txtMEOR.setCaretPosition(0); // for setting to start of text
+		
+		//ScrollPane surrounding credits
+		JScrollPane scrollPanecredits = new JScrollPane();
+		scrollPanecredits.setViewportBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
+		scrollPanecredits.setBounds(10, 150, 425, 110);
+		aboutpanel.add(scrollPanecredits);
+		scrollPanecredits.setViewportView(txtMEOR);
 	}
 }
